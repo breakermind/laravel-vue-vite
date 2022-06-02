f4f0ed1165b406af7c7af3193a0c59d46bfcf796
breakermind <m.luk.box@gmail.com>
2022-06-02 09:43:24 +0200

    Laravel Vue Vite SPA

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
+  linkActiveClass: "router-link-active",
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
@@ -16,8 +17,52 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    // {
+    //   path: '/params/:id/:code',
+    //   name: 'params',
+    //   component: () => import('../views/ParamsView.vue')
+    // },
+    // { path: '/activate/:id/:code', component: Activate, name: 'activate' },
+    // { path: '/panel', component: Profil, name: 'panel.profil', meta: { requiresAuth: true }},
+    // {
+    //  path: '/:catchAll(.*)',
+    //  component: () => import('../views/NotFound.vue'),
+    //  name: 'error.page'
+    // }
   ]
 })
+
+/*
+// Sample only
+// import { defineAsyncComponent } from 'vue'
+// import store from '../store/index.js'
+router.beforeEach((to) => {
+  // ✅ This will work make sure the correct store is used for the
+  // current running app
+  const main = useMainStore(pinia)
+  if (to.meta.requiresAuth && !main.isLoggedIn) return '/login'
+})
+router.beforeEach(async (to, from, next) => {
+  // Hide menu
+  store.dispatch('closeMenu')
+  // Authenticate with remember me token
+  await store.dispatch('user/auth/userLogged')
+  // Redirect
+  if(to.name == 'login' && store.state.user.auth.isAuthenticated) {
+    // Redirect to panel if logged
+    next({ name: 'panel' })
+  } else if (to.meta.requiresAuth && !store.state.user.auth.isAuthenticated) {
+    // Redirect if not logged
+    next({
+      name: 'login',
+      query: { redirected_from: to.fullPath }
+    })
+  } else {
+    // Next route
+    next()
+  }
+})
+*/
 
 export default router
---
+++ vue-project/src/views/ParamsView.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: "ParamsView",
+  data() {
+    return {
+      id: this.$route.params.id,
+      code: this.$route.params.code,
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is params page</h1>
+    <h4> {{ id }} {{ code }} </h4>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
