eaa49b3c5e467863ae2587e71f8b50b0d27fffa8
breakermind <m.luk.box@gmail.com>
2022-06-02 07:46:21 +0200

    Laravel Vue Vite SPA

---
+++ vue-project/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./views/**/*.{vue,js,ts,jsx,tsx}",
+    "./components/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
